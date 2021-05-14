7aa3519b896d907d8a889ede843a8f8231824f10
Audrey <a@gmail.com>
2021-05-14 12:17:56 +0300

    Bugfix

--- test2/test2.ino
+++ test2/test2.ino
@@ -9,6 +9,10 @@ void setup(){
   Serial.begin(9600);
   pinMode(led1,OUTPUT);
   pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+  pinMode(led5,OUTPUT);
+  pinMode(led6,OUTPUT);
   }
 
 void loop(){
